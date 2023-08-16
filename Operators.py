d7521f8ce4d60cdce15afff50ef72e60dc08f1cb
hilmi <hilmidemirbjk@gmail.com>
2023-08-17 02:32:30 +0300

    ObjectsAndDatas.py added

---
+++ Operators.py
@@ -0,0 +1,42 @@
+# Assigments
+'''
+# x = 5
+# y = 10
+# z = 20
+
+x, y, z = 5, 10, 15
+
+# x, y = y, x    x ve y değerleri değişir
+
+values = 1, 2, 3, 4 ,5
+print(values)
+print(type(values))
+x, y, *z = values  # eğer tuplemiz içinde az veya fazla eleman varsa atama yapamayız
+print(x, y, z)
+print(x,y ,z[2])
+'''
+
+x, y, z = 2, 5,10
+numbers = 1,5,7,10,6
+
+num1 = input("1. sayı")
+num2 = input("2. sayı")
+
+mult = int(num1) * int(num2)
+result = mult - (x + y +z )
+
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
